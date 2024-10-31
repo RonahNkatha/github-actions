2ffcb7cce4bcfe33db8613fb72cbef061e95302f
RonahNkatha <124624805+RonahNkatha@users.noreply.github.com>
2024-10-30 21:42:29 -0400

    Update connection.ts

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,8 +1,15 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techquiz');
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/techquiz")
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 export default mongoose.connection;
