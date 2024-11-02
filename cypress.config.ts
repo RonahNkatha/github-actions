70d60305fe8ad1093231a17ba3a6cd336d255b29
RonahNkatha <124624805+RonahNkatha@users.noreply.github.com>
2024-11-02 18:25:35 -0400

    fixed cypress configs

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,18 +1,20 @@
-import { defineConfig } from 'cypress';
-import viteConfig from './vite.config';
+import { defineConfig } from "cypress";
+import viteConfig from "./vite.config";
 
 export default defineConfig({
   component: {
+    specPattern: "cypress/component/*.cy.jsx",
     port: 5173,
     devServer: {
-      framework: 'react',
-      bundler: 'vite',
+      framework: "react",
+      bundler: "vite",
       viteConfig,
     },
   },
 
   e2e: {
-    baseUrl: 'http://localhost:3001',
+    baseUrl: "http://localhost:3001",
+    specPattern: "cypress/support/e2e.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
