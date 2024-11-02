58e788b6202c2b7bb545cb0cbe7a86a6a49c2165
RonahNkatha <124624805+RonahNkatha@users.noreply.github.com>
2024-11-02 18:28:28 -0400

    fixed server

--- cypress.config.ts
+++ cypress.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   },
 
   e2e: {
-    baseUrl: "http://localhost:3001",
+    baseUrl: "https://github-actions-cig4.onrender.com",
     specPattern: "cypress/support/e2e.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
